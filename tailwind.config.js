fd39e575a280d45f8f06bd8b68d60f35bb52d68d
Sachin Mour <rtr.sachinmour@gmail.com>
2021-12-12 02:30:36 +0000

    working example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [path.join(__dirname, "./src/**/*.(js|jsx|ts|tsx)")],
   theme: {
     extend: {},
   },
